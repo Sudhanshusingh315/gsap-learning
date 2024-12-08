3a679c93788201b38bf080038990a5431afcec1e
Sudhanshu singh <ss826063@gmail.com>
2024-12-08 21:59:50 +0530

    lecture-2 starting

--- part-1/script.js
+++ part-1/script.js
@@ -1,5 +1,26 @@
-gsap.to(".box",{
-    x:1000,
+// gsap.to(".box",{
+//     x:1000,
+//     duration:2,
+//     delay:1
+// })
+
+// gsap.from(".box2",{
+//     y:500,
+//     opacity:0,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     borderRadius:"50%",
+
+// })
+
+gsap.from("h1",{
+    opacity:0,
+    color:"red",
     duration:2,
-    delay:1
+    delay:1,
+    stagger:1,
+    ease:"sine.out",
+   
 })
